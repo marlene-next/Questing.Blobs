7fe292899e966bb1daa97602da9673313b9198d9
Marlene Mayr <marlene.mayr@nextgames.com>
2022-11-18 09:03:19 +0200

    Inherit BlobdexPresenter from PresenterBase

--- blobs/Presentation/States/BlobdexPresenter.cs
+++ blobs/Presentation/States/BlobdexPresenter.cs
@@ -2,29 +2,23 @@
 
 namespace blobs.Presentation.States;
 
-public class BlobdexPresenter : IPresenter
+public class BlobdexPresenter : PresenterBase
 {
-    private readonly IStateMachine _stateMachine;
     private InputHandler _inputHandler;
 
-    public BlobdexPresenter(IStateMachine stateMachine)
-    {
-        stateMachine.ThrowIfNull(nameof(stateMachine));
+    public BlobdexPresenter(IStateMachine stateMachine) : base(stateMachine) { }
 
-        _stateMachine = stateMachine;
-    }
-
-    public void Initialize(IViewModel viewModel)
+    public override void Initialize(IViewModel viewModel)
     {
         _inputHandler = InputHandler.Create().Add(ConsoleKey.B, "back");
     }
 
-    public void Present()
+    public override void Present()
     {
         Console.WriteLine(nameof(BlobdexPresenter));
 
         var key = _inputHandler.WaitForKey();
         if (key == ConsoleKey.B)
-            _stateMachine.ChangeState(StateNameConstants.MainMenuState);
+            StateMachine.ChangeState(StateNameConstants.MainMenuState);
     }
 }
